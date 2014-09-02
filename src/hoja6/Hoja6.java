6f9634b0d65c90014c4bd0f637d43b3b595bc477
diegojacobs <diegojacobs9595@gmail.com>
2014-09-02 08:29:53 -0600

    main

--- src/hoja6/Hoja6.java
+++ src/hoja6/Hoja6.java
@@ -29,33 +29,35 @@ public class Hoja6 {
         Set java;
         Set web;
         
+        int tipo = 0;
         System.out.println("Escoge que tipo de conjunto desea trabajar: ");
         System.out.println("1. HashSet");
         System.out.println("2. TreeSet");
-        System.out.print("3. ");
+        System.out.println("3. LinkedHashSet");
+        tipo = _in.nextInt();
         int opcion = 0;
         while (opcion != 3)
         {
-            System.out.print("Escoge una opcion: ");
-            System.out.print("1. Ingresar nueva persona");
-            System.out.print("2. Desplegar conjunto mas grande");
-            System.out.print("3. Salir");
+            System.out.println("Escoge una opcion: ");
+            System.out.println("1. Ingresar nueva persona");
+            System.out.println("2. Desplegar conjunto mas grande");
+            System.out.println("3. Salir");
             opcion = _in.nextInt();
             
             if (opcion == 1)
             {
                 String name;
                 int opcion2 = 0;
-                System.out.print("Ingrese Nombre: ");
+                System.out.println("Ingrese Nombre: ");
                 name = _in.nextLine();
-                System.out.print("Indique de que conjuntos es parte: ");
-                System.out.print("1. Web");
-                System.out.print("2. Java");
-                System.out.print("3. Celular");
-                System.out.print("4. Web y Java");
-                System.out.print("5. Web y Celular");
-                System.out.print("6. Java y Celular");
-                System.out.print("7. Web, Java y Celular");
+                System.out.println("Indique de que conjuntos es parte: ");
+                System.out.println("1. Web");
+                System.out.println("2. Java");
+                System.out.println("3. Celular");
+                System.out.println("4. Web y Java");
+                System.out.println("5. Web y Celular");
+                System.out.println("6. Java y Celular");
+                System.out.println("7. Web, Java y Celular");
                 opcion2 = _in.nextInt();
             }
         }
