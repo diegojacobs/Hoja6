a3640050af0d4cd764d89bc5871e46311659dfc1
diegojacobs <diegojacobs9595@gmail.com>
2014-09-02 08:18:11 -0600

    main

--- src/hoja6/Hoja6.java
+++ src/hoja6/Hoja6.java
@@ -6,9 +6,12 @@
 
 package hoja6;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Diego Jacobs
+ * @author Diego Jacobs 13160
+ * @author Roberto Chiroy   13027
  */
 public class Hoja6 {
 
@@ -17,6 +20,35 @@ public class Hoja6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner _in  = new Scanner(System.in);
+        
+        int opcion = 0;
+        while (opcion != 3)
+        {
+            System.out.print("Eliga una opcion: ");
+            System.out.print("1. Ingresar nueva persona");
+            System.out.print("2. Desplegar conjunto mas grande");
+            System.out.print("3. Salir");
+            opcion = _in.nextInt();
+            
+            if (opcion == 1)
+            {
+                String name;
+                int opcion2 = 0;
+                System.out.print("Ingrese Nombre: ");
+                name = _in.nextLine();
+                System.out.print("Eliga de que conjuntos es parte: ");
+                System.out.print("1. Web");
+                System.out.print("2. Java");
+                System.out.print("3. Celular");
+                System.out.print("4. Web y Java");
+                System.out.print("5. Web y Celular");
+                System.out.print("6. Java y Celular");
+                System.out.print("7. Web, Java y Celular");
+                opcion2 = _in.nextInt();
+            }
+        }
     }
     
 }
