f90c5871a3ff6c32b058be97774e0b286c679b48
diegojacobs <diegojacobs9595@gmail.com>
2014-09-02 08:09:50 -0600

    factory

---
+++ src/hoja6/Operaciones.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hoja6;
+
+/**
+ *
+ * @author Diego Jacobs
+ */
+public class Operaciones {
+    
+}
